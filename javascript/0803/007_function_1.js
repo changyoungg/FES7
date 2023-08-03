95645ca2c0a1cbfb3a314d7dd156860a06b4f1fd
chang0 <mir970@naver.com>
2023-08-03 15:55:53 +0900

    0803

---
+++ javascript/0803/007_function_1.js
@@ -0,0 +1,26 @@
+// function add(x, y) {
+//     return x + y
+// }
+
+// console.log(add(1, 2));
+
+// function blye(x, y) {
+//     return x - y
+// }
+
+// console.log(blye(2, 1));
+
+// function output(word) {
+//     console.log(word);
+//     return word.split('');
+// }
+
+// console.log(output('asddqwd'));
+
+// function multiNum (x, y) {
+//     let add = (x + y);
+//     let multi = add * add ;
+//     return multi;
+// }
+
+// console.log(multiNum(3, 4));
---
+++ javascript/0803/009_신입사원주의코드.js
@@ -0,0 +1,12 @@
+function test() {
+    console.log('hello')
+    console.log('hello')
+    console.log('hello')
+}
+
+function test() {
+    console.log('hello')
+}
+
+test()
+// 뒤에 있는 hello 한개만 출력됨
